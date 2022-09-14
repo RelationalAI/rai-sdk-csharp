ee9d734c474737c9ef911dad952e5f55f542d418
helmi.nour <helmiinour@gmail.com>
2022-09-14 05:27:07 +0100

    fix test

--- RelationalAI.Test/ExecuteAsync.cs
+++ RelationalAI.Test/ExecuteAsync.cs
@@ -15,6 +15,7 @@ namespace RelationalAI.Test
         public static string Uuid = Guid.NewGuid().ToString();
         public static string Dbname = $"csharp-sdk-{Uuid}";
         public static string EngineName = $"csharp-sdk-{Uuid}";
+
         [Fact]
         public async Task ExecuteAsyncTest()
         {
@@ -36,19 +37,37 @@ namespace RelationalAI.Test
             var recordBatch = new RecordBatch
             (
                 builder.Build(),
-                new List<IArrowArray>
+                new List<Int64Array>
                 {
                     new Int64Array.Builder().AppendRange(new List<long> { 1, 2, 3, 4, 5 }).Build(),
                     new Int64Array.Builder().AppendRange(new List<long> { 1, 4, 9, 16, 25 }).Build(),
                     new Int64Array.Builder().AppendRange(new List<long> { 1, 8, 27, 64, 125 }).Build(),
                     new Int64Array.Builder().AppendRange(new List<long> { 1, 16, 81, 256, 625 }).Build()
                 },
-                4
+                5
             );
 
             var table = Table.TableFromRecordBatches(recordBatch.Schema, new List<RecordBatch> { recordBatch });
 
-            Assert.Equal(table, rsp.Results[0].Table);
+            for (int i = 0; i < table.ColumnCount; i++)
+            {
+                for (int j = 0; j < table.Column(i).Data.ArrayCount; j++)
+                {
+                    for (int k = 0; k < (table.Column(i).Data.Array(j) as Int64Array).Length; k++)
+                    {
+                        var expected = (table.Column(i).Data.Array(j) as Int64Array).GetValue(k).Value;
+                        var actual = (rsp.Results[0].Table.Column(i).Data.Array(j) as Int64Array).GetValue(k).Value;
+                        Assert.Equal(expected, actual);
+                    }
+                }
+            }
+
+            // mock proto metadata
+            var metadata = MetadataInfo.Parser.ParseFrom(File.ReadAllBytes("../../../metadata.pb"));
+            Assert.Equal(metadata.Relations[0].RelationId.Arguments, rsp.Results[0].Metadata.Arguments);
+
+            // mock problems
+            Assert.Equal(new List<object>(), rsp.Problems);
         }
 
         public override async Task DisposeAsync()
