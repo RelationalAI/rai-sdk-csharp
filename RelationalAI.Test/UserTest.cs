73fcbd358f9536fc866715756f487632c4b9a756
helmi.nour <helmiinour@gmail.com>
2022-11-17 14:36:56 +0100

    enable back user and oauth client tests

--- RelationalAI.Test/UserTest.cs
+++ RelationalAI.Test/UserTest.cs
@@ -11,8 +11,7 @@ namespace RelationalAI.Test
         public static string Uuid = Guid.NewGuid().ToString();
         public static string UserEmail = $"csharp-sdk-{Uuid}@example.com";
 
-        // TODO: Keep it disabled until we fix the Auth0 API rate limiting issue
-        //[Fact] 
+        [Fact]
         public async Task TestUser()
         {
             var client = CreateClient();
