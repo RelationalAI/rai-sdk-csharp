247ec3d23a2a123353f7ef3aacaa8e240c13f2c0
helmi.nour <helmiinour@gmail.com>
2023-01-24 14:31:29 +0100

    test output helper doesn't support write only

--- RelationalAI.Test/RAITestTraceListener.cs
+++ RelationalAI.Test/RAITestTraceListener.cs
@@ -13,7 +13,10 @@ namespace RelationalAI.Test
         }
         public override void Write(string message)
         {
-            WriteLine(message);
+            // ITestOutputHelper has no Write function
+            // using WriteLine for the moment until we figure
+            // out a better way to this
+            testOutputHelper.WriteLine(message);
         }
 
         public override void WriteLine(string message)
