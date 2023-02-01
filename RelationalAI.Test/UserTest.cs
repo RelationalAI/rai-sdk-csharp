b4ab26d0983101871c40ab6280fc57f065d65b2e
helmi.nour <helmiinour@gmail.com>
2023-02-01 15:42:49 +0100

    fix warning: missing file header

--- RelationalAI.Test/UserTest.cs
+++ RelationalAI.Test/UserTest.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
 
