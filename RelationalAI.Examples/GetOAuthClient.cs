459c7f6afd41e4cbc70322e16f004b9e64c0b70d
helmi.nour <helmiinour@gmail.com>
2023-02-03 13:55:35 +0100

    cleanup

--- RelationalAI.Examples/GetOAuthClient.cs
+++ RelationalAI.Examples/GetOAuthClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using RelationalAI;
 
 namespace RelationalAI.Examples
